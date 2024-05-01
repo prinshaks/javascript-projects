91bd574fda40831e9936ae37ac0f36f91a31ac07
prinshaks <prinshasudha@gmail.com>
2024-04-30 22:05:39 -0500

    space shuttle studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,4 +1,20 @@
 // Initialize Variables below
+let date=	"Monday 2019-03-18";
+let time=	"10:05:34 AM";
+let astronautCount=	7;
+let astronautStatus=	"ready";
+let averageAstronautMassKg=	80.7;
+let crewMassKg=	astronautCount * averageAstronautMassKg;
+let fuelMassKg=	760000;
+let shuttleMassKg=	74842.31;
+let totalMassKg=	crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit=	850000;
+let fuelTempCelsius=	-225;
+let minimumFuelTemp=	-300;
+let maximumFuelTemp=	-150;
+let fuelLevel=	100;
+let weatherStatus=	"clear";
+let preparedForLiftOff=	true;
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
@@ -12,4 +28,37 @@
 
 // add logic below to verify the weather status is clear
 
+preparedForLiftOff=false;
+if (!(astronautCount >7)){
+    if (astronautStatus==="ready"){
+       if (totalMassKg<maximumMassLimit){
+        if( !(fuelTempCelsius <-300) || !(fuelTempCelsius >-150)){
+            if(fuelLevel===100){
+                if(weatherStatus==="clear"){
+                    preparedForLiftOff=true;
+                }
+            }
+        }
+       }
+    }
+}
+
 // Verify shuttle launch can proceed based on above conditions
+if(preparedForLiftOff===true){
+console.log("All systems are a go! Initiating space shuttle launch sequence.");
+console.log("---------------------------------------------------------------");
+console.log("Date: " + date);
+console.log("Time: " + time);
+console.log("Astronaut Count: " + astronautCount);
+console.log("Crew Mass: " + crewMassKg + " kg");
+console.log("Fuel Mass: " + fuelMassKg + " kg");
+console.log("Shuttle Mass: "+ shuttleMassKg +" kg");
+console.log("Total Mass: "+totalMassKg +" kg");
+console.log("Fuel Temperature: "+fuelTempCelsius + " C");
+console.log("Weather Status: " + weatherStatus);
+console.log("---------------------------------------------------------------");
+console.log("Have a safe trip astronauts!");
+}
+else {
+console.log("SHUTDOWN");
+}
