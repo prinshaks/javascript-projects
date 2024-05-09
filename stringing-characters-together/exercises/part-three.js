34548b9f6606cd71adde2360389e2c3afa27f0c2
prinshaks <prinshasudha@gmail.com>
2024-05-08 22:55:57 -0500

    strings exercise

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,10 +3,13 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+console.log(language.slice(0,1)+language.slice(4,5));
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+console.log(language[0] + language[4]);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+console.log(`The abbreviation for 'JavaScript' is '${language[0] + language[4]}'.`);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
